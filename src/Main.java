77ad79d10dfca1b13e9bacd55b777754abda709f
AntonShiryaev <98731055+AntonShiryaev@users.noreply.github.com>
2023-02-28 03:44:48 +0500

    byte short int long, float double

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //4.13 Задание вывести минимальное и максимальное значение типов чисел
+        byte BYTE_MIN=Byte.MIN_VALUE;
+        System.out.println("Byte minimal value: "+BYTE_MIN);
+        byte BYTE_MAX=Byte.MAX_VALUE;
+        System.out.println("Byte maximal value: "+BYTE_MAX);
+
+        int INT_MIN=Integer.MIN_VALUE;
+        System.out.println("Integer minimal value: "+INT_MIN);
+        int INT_MAX=Integer.MAX_VALUE;
+        System.out.println("Integer maximal value: "+INT_MAX);
+
+        short SHORT_MIN=Short.MIN_VALUE;
+        System.out.println("Short minimal value: "+SHORT_MIN);
+        short SHORT_MAX=Short.MAX_VALUE;
+        System.out.println("Short maximal value: "+SHORT_MAX);
+
+        long LONG_MIN=Long.MIN_VALUE;
+        System.out.println("Long minimal value: "+LONG_MIN);
+        long LONG_MAX=Long.MAX_VALUE;
+        System.out.println("Long maximal value: "+LONG_MAX);
+
+        float FLOAT_MIN=Float.MIN_VALUE;
+        System.out.println("Float minimal value: "+FLOAT_MIN);
+        float FLOAT_MAX=Float.MAX_VALUE;
+        System.out.println("Float maximal value: "+FLOAT_MAX);
+
+        double DOUBLE_MIN=Double.MIN_VALUE;
+        System.out.println("Double minimal value: "+DOUBLE_MIN);
+        double DOUBLE_MAX=Double.MAX_VALUE;
+        System.out.println("Double maximal value: "+DOUBLE_MAX);
+
+    }
+}
